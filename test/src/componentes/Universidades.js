81bbb3c9bf400f259ff1c485f132137cc5489b94
alberto_combatti <combattialberto@gmail.com>
2021-11-18 16:14:16 -0500

    post universidades

--- test/src/componentes/Universidades.js
+++ test/src/componentes/Universidades.js
@@ -4,10 +4,18 @@ import "./universidadesStyle.css";
 export default function Universidades() {
     return(
         <div id="form-main" href="universidadesStyle.css">
+          <ul class="flex-container">
+  <li class="flex-item">
+    <h1 class="u">Universidad de la Sabana</h1>
+    <h1 class="user">Usuario</h1>
+    <p class="commentPost">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </li>
+</ul>
         <div id="form-div">
+
           <form class="form" id="form1">
-            
-            
+
+          
             
             <p class="text">
               <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comentario"></textarea>
